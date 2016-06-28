ff989929043ba92a2b61a59eae54ce7f7bb6cfe1
davesarr <davesarr@hotmail.com>
2016-06-28 17:48:00 -0400

    Update main.js

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,19 @@
 console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+if (cardOne === cardTwo){
+	alert("you found a match!");
+};
+else {
+	alert("Sorry, try again.")
+
+	};
+if (cardThree === cardFour){
+	alert("you found a match!")
+};
+else {
+	alert("Sorry, try again.");
+};
+
